1f8236d74e5b23cae430d1c2beb56497bda710ff
Stephan Bezoen <stephan@mediamonks.com>
2023-01-09 09:17:25 +0100

    Added companion for Instant so the companion object becomes available to Swift Now able to instantiate an Asset instance in Swift

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,11 +2,18 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-
+    let type:AssetType
+    let datex:Kotlinx_datetimeInstant
+    let asset:Asset
+    init() {
+        type = AssetType.image
+        datex = Kotlinx_datetimeInstant.Companion().fromEpochMilliseconds(epochMilliseconds: Int64(Date().timeIntervalSince1970) * 1000)
+        
+        asset = Asset(id: "1234", url: "1234", addedTimestamp: datex, type: type)
+    }
     
 	var body: some View {
-        var asset:Asset = Asset(id: <#T##Any#>, url: <#T##String#>, addedTimestamp: <#T##Kotlinx_datetimeInstant#>, type: <#T##AssetType#>)
-		Text("greet")
+		Text("greet \(asset)")
 	}
 }
 
---
+++ shared/src/iosMain/kotlin/nl/acidcats/imageviewer/InstantExt.kt
@@ -0,0 +1,6 @@
+package nl.acidcats.imageviewer
+
+import kotlinx.datetime.Instant
+
+@Suppress("unused")
+fun Instant.Companion.dummy():Nothing = TODO()
