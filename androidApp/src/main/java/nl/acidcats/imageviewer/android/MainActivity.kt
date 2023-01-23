e1d3c1c48a6ce3b7a301ad4f5e52336bd935d57c
Stephan Bezoen <stephan@mediamonks.com>
2023-01-23 11:50:47 +0100

    Added edge-to-edge support

--- androidApp/src/main/java/nl/acidcats/imageviewer/android/MainActivity.kt
+++ androidApp/src/main/java/nl/acidcats/imageviewer/android/MainActivity.kt
@@ -1,6 +1,10 @@
 package nl.acidcats.imageviewer.android
 
+import android.content.res.Configuration
+import android.content.res.Resources
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +18,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import nl.acidcats.imageviewer.android.ui.AssetViewer
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -22,6 +29,8 @@ class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        edgeToEdge(window, findViewById(android.R.id.content), theme)
+
         super.onCreate(savedInstanceState)
 
         setContent {
@@ -49,6 +58,23 @@ class MainActivity : ComponentActivity() {
         super.onStart()
 
         window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+    }
+
+    private fun edgeToEdge(window: Window, view: View, theme: Resources.Theme) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val resources = view.resources
+        val transparent = ResourcesCompat.getColor(resources, android.R.color.transparent, theme)
+        val isDarkMode = isDarkMode(resources)
+        window.statusBarColor = transparent
+        window.navigationBarColor = transparent
+        val controller = WindowInsetsControllerCompat(window, view)
+        controller.isAppearanceLightStatusBars = !isDarkMode
+        controller.isAppearanceLightNavigationBars = !isDarkMode
+    }
+
+    private fun isDarkMode(resources: Resources): Boolean {
+        return (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) ==
+                Configuration.UI_MODE_NIGHT_YES
     }
 }
 
